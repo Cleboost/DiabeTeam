725734a6264c8c05bc595f786f200348d0ea528c
Cleboost <clement.balarot@gmail.com>
2026-02-10 11:59:03 +0100

    feat: Implement dynamic base path for GitHub Pages preview deployments.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	base: "/DiabeTeam/",
+	base: process.env.BASE_PATH || "/DiabeTeam/",
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
