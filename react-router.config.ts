8ceae15889e4ac23a6b2c29de4d51bdf6f3848b0
Cleboost <clement.balarot@gmail.com>
2026-02-10 16:27:07 +0100

    chore: Set default base path to root in router and Vite configurations.

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
 	ssr: false,
-	basename: process.env.BASE_PATH || "/DiabeTeam/",
+	basename: process.env.BASE_PATH || "/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	base: process.env.BASE_PATH || "/DiabeTeam/",
+	base: process.env.BASE_PATH || "/",
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
